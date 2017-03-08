cb50cec9da0c8637292098e535c0ab45ac365329
Mehdhi <mehdhi@cinergix.com>
2017-03-08 17:23:19 +0530

    Added functionality to read json and configuration files and also adding and storing the templates for generrating configure files

---
+++ src/index.js
@@ -0,0 +1,102 @@
+var fs = require( 'fs' ); //importing file system
+
+/**
+ * This consists functionalities related configuration management for application with 
+ * multiple environment. It utilizes gulp to achieve the task
+ * 
+ * @param   {String} configFilePattern The string pattern which identifies the files 
+ *          of configuration for specific environment. The index of the file is denoted 
+ *          by positioning {KEY_ENV} in the pattern 
+ */
+var EnvConfig = ( configFilePattern ) => {
+    
+    /**
+     * This is key which is used to identify environment name
+     * index from the configuration file pattern.
+     */
+    const KEY_ENV = '@@ENV';
+
+    /**
+     * This variable {Map}<Key, Value> holds data of the templates which needs
+     * configurations to be applied 
+     */
+    let templates;
+
+    /**
+     * This function reads a json file and return an Object
+     * @param {String} fileName
+     * @returns Object
+     */
+    function readJSON( fileName ) {
+        if( fileName ) {
+            return JSON.parse( fs.readFileSync( fileName, 'utf8' ) );
+        }
+        return undefined;
+    }
+
+    /**
+     * This function reads a configuration json file and return an Object
+     * @param {String} env
+     * @returns Object
+     */
+    function readConfig( env ) {
+        try {
+            return readJSON( getConfigFilePath( env ) );
+        } catch ( err ){
+            gutil.log( gutil.colors.red( '=> Could find file for `' + env + '` environment!' ) );
+        }
+        return undefined;
+    }
+
+    /**
+     * This functions give the respected path of a config file for a given environment
+     * @param {String} env
+     * @returns {String} File path for configuration file
+     */
+    function getConfigFilePath( env ){
+        let filePath = undefined;
+        if( validateConfigFilePattern() && env ) {
+            let pathArray = path.split( KEY_ENV );
+            if( pathArray && pathArray.length == 2 ) {
+                filePath = pathArray[0] + env + pathArray[1];
+            }
+        } else {
+             gutil.log( gutil.colors.red( '=> Invalid path pattern for config file or invalid environment.' ) );
+        }
+        return filePath;
+    }
+
+    /**
+     * This function checks the pattern of config file is valid
+     * @returns boolean True if it matches the expected pattern
+     */
+    function validateConfigFilePattern() {
+        let pattern = new RegExp( '^(.\/[a-zA-Z_\-0-9\.]+)+(@@ENV)\.(json)$' );
+        return ( configFilePattern && pattern.test( configFilePattern ) );
+    }
+
+    return {
+        
+        /**
+         * This method add details of template file and output location into the template map 
+         */
+        addTemplate: ( templateFile, outputFile ) => {
+            if( !templates ) {
+                templates = new Map();
+            }
+            templates.set( templateFile, outputFile );
+        },
+
+        /**
+         * This method remove details of template file from the template map 
+         */
+        removeTemplate: ( templateFile ) => {
+            if( templates ){
+                return templates.delete( templateFile );
+            }
+            return false;
+        }
+    }
+}
+
+module.exports = EnvConfig;
