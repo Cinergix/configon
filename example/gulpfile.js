07629ba3e6fcbea797f693c99cf7ddcae72cefa4
Mehdhi <mehdhi@cinergix.com>
2017-03-10 16:42:47 +0530

    Added config.xml template example

--- example/gulpfile.js
+++ example/gulpfile.js
@@ -2,11 +2,11 @@
  * This gulpfile is to demonstrate the capability of the configon
  */
 var gulp = require('gulp');
-var EnvConfig = require('../src/index.js');
+var EnvConfig = require('configon');
 
-const config = new EnvConfig( './example/config/config.@@ENV.json' )
-            .addTemplate( './example/config/template.ts', './bin/config.ts' )//adding template 1
-            .addTemplate( './example/config/template-config.xml', './bin/config.xml' )//adding template 2
+const config = new EnvConfig( './config/config.@@ENV.json' )
+            .addTemplate( './config/template.ts', './bin/config.ts' )//adding template 1
+            .addTemplate( './config/template-config.xml', './bin/config.xml' )//adding template 2
             .addTemplate( './example/some.file', './bin/some.file' )//adding template 3
             .removeTemplate( './example/some.file' );//removing template 3
 
