632f43189fa29e46bbd4d825df205cc71e5f6851
Mehdhi <mehdhi@cinergix.com>
2017-03-13 18:03:17 +0530

    Improved example code

--- example/gulpfile.js
+++ example/gulpfile.js
@@ -1,27 +1,34 @@
 /**
  * This gulpfile is to demonstrate the capability of the configon
  */
-var gulp = require('gulp');
-var EnvConfig = require('configon');
+var gulp = require( 'gulp' );
+var EnvConfig = require( '../src/index.js' );
 
-const config = new EnvConfig( './config/config.@@ENV.json' )
-            .addTemplate( './config/template.ts', './bin/config.ts' )//adding template 1
-            .addTemplate( './config/template-config.xml', './bin/config.xml' )//adding template 2
-            .addTemplate( './example/some.file', './bin/some.file' )//adding template 3
-            .removeTemplate( './example/some.file' );//removing template 3
+const config = new EnvConfig( './example/config/config.@@ENV.json' )
+            .addTemplate( './example/config/template.ts', './app/config.ts' )//adding template 1
+            .addTemplate( './example/config/template-config.xml', './config.xml' );//adding template 2
+
 
 /**
- * Testing environment based config example
- * Example commands
- * gulp config --env:dev - sets custom environment
- * gulp config --ip:192.168.0.45 - sets custom ip
+ * Sample task that builds the application
+ * Sample  commands to change environment
+ * gulp build --env:production - sets the environment
+ * gulp build --ip:192.168.0.45 - sets the IP Address
  * 
- * or TRY setting environment variable
- *  ( SET | export ) NODE_ENV=dev 
+ * Alternatively
+ *  ( SET | export ) NODE_ENV=production
+ *  gulp build
+ */
+gulp.task('build', ['config'],  function() {
+    //....
+    //App building code
+    //....
+});
+
+
+/**
+ * Sample task that applies the configuration
  */
 gulp.task('config', function() {
-    
-    var defaultEnv = 'dev';
-    config.build( defaultEnv );
-    
+    config.build( 'development' );
 });
